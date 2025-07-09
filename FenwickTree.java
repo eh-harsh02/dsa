1bd0b0f9c4f17ae0e74a1eeccee6ecb301aa373c
Harsh Raj <harshraj0381@gmail.com>
2025-07-09 14:59:39 +0530

    Updated and Added SegmentTreeArrays

--- FenwickTree.java
+++ FenwickTree.java
@@ -1,45 +1,42 @@
 public class FenwickTree {
-    int[] arr; int[] bits; int N;
-
+    int[] arr; int N;
+    int[] bits;
     FenwickTree(int N) {
         this.N = N;
         this.arr = new int[N];
         this.bits = new int[N + 1];
     }
-    
-    public void update(int idx, int elt){
-        while(idx <= N){
+    // T(N) --> log(N)
+    public void update(int idx, int elt) {
+        while( idx <= N ) {
             bits[idx] += elt;
-            idx += idx & -idx;
+            idx += idx & (-idx);
         }
     }
-
-    public int range(int idx){
+    public int range(int idx) {     // sum(range(1, idx))
         int sum = 0;
-        while(idx>0){
+        // calculate
+        while( idx > 0 ) {
             sum += bits[idx];
-            idx -= idx & -idx;
+            idx -= idx & (-idx);
         }
         return sum;
     }
-
-    public int rangeQuery(int i, int j){
+    // T(N) = O(1)
+    public int rangeQuery(int i, int j) {
         return range(j) - range(i-1);
     }
-    
     public static void main(String[] args) {
+        int N = 8;
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
-        int N = arr.length;
         FenwickTree fenwickTree = new FenwickTree(N);
-        for(int i = 1; i <= N; i++){
+        for(int i=1; i <= N; i++)
             fenwickTree.update(i, arr[i-1]);
-        }
 
-        for(int bit : fenwickTree.bits){
+        for(int bit: fenwickTree.bits)
             System.out.print(bit + " ");
-        }
         System.out.println();
-        System.out.println(fenwickTree.range(3));
-        System.out.println(fenwickTree.rangeQuery(2, 3));
+        System.out.println( fenwickTree.range(5) );             // 150
+        System.out.println( fenwickTree.rangeQuery(3, 6) );     // 230 (30 + 40 + 50 + 60)
     }
 }
--- SegmentTree.java
+++ SegmentTree.java
@@ -1,5 +1,93 @@
+import java.util.Scanner;
 public class SegmentTree {
+    Node root;
+    static class Node {
+        int data;
+        int startInterval; int endInterval;
+        Node left, right;
+        Node(int startIndex, int endIndex) {
+            this.startInterval = startIndex;
+            this.endInterval = endIndex;
+        }
+    }
+    SegmentTree(int[] arr) {
+        this.root = buildTree(arr, 0, arr.length - 1);
+    }
+    // T(N) = O(N)
+    public Node buildTree(int[] arr, int start, int end) {
+        if(start == end) {
+            Node leaf = new Node(start, end);
+            leaf.data = arr[start];
+            return leaf;
+        }
+        Node node = new Node(start, end);
+        int mid = (start + end) / 2;
+        node.left = buildTree(arr, start, mid);
+        node.right = buildTree(arr, mid + 1, end);
+        node.data = node.left.data + node.right.data;
+        return node;
+    }
+    // T(N) = O(log N)
+    public int rangeQuery(int startInterval, int endInterval) {
+        return rangeQuery(root, startInterval, endInterval);
+    }
+    public int rangeQuery(Node root, int sIdx, int eIdx) {
+        if(root == null) return 0;
+        else if(sIdx <= root.startInterval && eIdx >= root.endInterval)
+            return root.data;
+        else if(root.startInterval > eIdx || root.endInterval < sIdx)
+            return 0;
+        else return rangeQuery(root.left, sIdx, eIdx) + rangeQuery(root.right, sIdx, eIdx);
+    }
+    // T(N) =
+    public int update(int idx, int elt) {
+        return update(root, idx, elt);
+    }
+    public int update(Node node, int idx, int elt) {
+        if(idx >= node.startInterval && idx <= node.endInterval) {
+            if(idx == node.startInterval && idx == node.endInterval) {
+                // update
+                node.data = elt;
+                return node.data;
+            } else {
+                // backtrack
+                int leftSum = update(node.left, idx, elt);
+                int rightSum = update(node.right, idx, elt);
+                node.data = leftSum + rightSum;
+                return node.data;
+            }
+        }
+        return node.data;
+    }
+    // T(N) = O(N)
+    public void display() {
+        display(this.root);
+    }
+    public void display(Node root) {
+        String str = "";
+        str = str + "Interval - [" + root.startInterval + " - " + root.endInterval + "] | Data - " + root.data + " | ";
+        System.out.println(str);
+        if(root.left != null) {
+            display(root.left);
+        }
+        if(root.right != null) {
+            display(root.right);
+        }
+    }
     public static void main(String[] args) {
-        
+        /*
+        8
+        2 3 5 -6 12 1 4 9
+         */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i < N; i++) arr[i] = sc.nextInt();
+        SegmentTree segmentTree = new SegmentTree(arr);
+        segmentTree.display();
+        System.out.println( segmentTree.rangeQuery(3, 6) );                     // 11
+        segmentTree.update(3, -10);
+        segmentTree.display();
+        System.out.println( segmentTree.rangeQuery(3, 6) );                     // 7
     }
 }
