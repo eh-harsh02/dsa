5d6ae94ced7a85416dc8ab67535164cf5d73fc5f
Harsh Raj <harshraj0381@gmail.com>
2025-08-11 01:36:00 +0530

    Updated TowersOfHanoi problem

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -1,13 +1,16 @@
 public class TowersOfHanoi {
     public static void towerOfHanoi (int n, char source_rod, char destination_rod, char aux_rod) {
-        if(n == 0) return;
+        if(n == 1) {
+            System.out.println("Move disk " + n + " "+source_rod + " --> " + destination_rod);
+            return;
+        }
         towerOfHanoi(n - 1, source_rod, aux_rod, destination_rod);
         System.out.println("Move disk " + n + " "+source_rod + " --> " + destination_rod);
         towerOfHanoi(n-1, aux_rod, destination_rod, source_rod);
 
     }
     public static void main(String[] args) {
-        int n = 2;
+        int n = 1;
         towerOfHanoi(n, 'A', 'B', 'C');
     }
 }
